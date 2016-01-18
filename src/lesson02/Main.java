044fdd955017eb714ec9bf6439b8cd840d50c620
papa <mbr0969@gmail.com>
2016-01-19 02:34:10 +0300

    implement equals() and hachCode

--- src/lesson02/Main.java
+++ src/lesson02/Main.java
@@ -1,7 +1,18 @@
 package lesson02;
 
+import loc.linux.webapp.model.Link;
+
 /**
  * Created by papa on 19.01.16.
  */
 public class Main {
+    public static void main(String[] args) {
+
+        Link link1 = new Link("Papa","htts://vk.com/id14074974");
+        Link link2 = link1;
+        Link link3 = new Link(link1);
+        System.out.println(link3.getClass());
+        System.out.println(link1.equals(link2));
+        System.out.println(link3.equals(link1));
+    }
 }
