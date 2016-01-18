fcde089fde70abf167870d09ece49fa429c28be7
papa <mbr0969@gmail.com>
2016-01-19 01:24:47 +0300

    add to Link and Resume

---
+++ src/loc/linux/webapp/model/Link.java
@@ -0,0 +1,9 @@
+package loc.linux.webapp.model;
+
+/**
+ * Created by papa on 19.01.16.
+ */
+public class Link {
+    private String name;
+    private String url;
+}
---
+++ src/loc/linux/webapp/model/Resume.java
@@ -0,0 +1,7 @@
+package loc.linux.webapp.model;
+
+/**
+ * Created by papa on 19.01.16.
+ */
+public class Resume {
+}
