8f51b81194b0e2e0815256070c1e4befcf2a0133
papa <mbr0969@gmail.com>
2016-01-19 19:39:13 +0300

    Lesson03

---
+++ src/lesson03/Main.java
@@ -0,0 +1,14 @@
+package lesson03;
+
+import loc.linux.webapp.model.Contact;
+import loc.linux.webapp.model.ContactType;
+
+/**
+ * Created by papa on 19.01.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Contact contact = new Contact(ContactType.MOBILE,"+79045598062");
+        System.out.println(contact);
+    }
+}
