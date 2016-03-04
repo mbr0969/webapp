ebaf24149d7fc626574205076bfea16b5b8a2ca2
papa <mbr0969@gmail.com>
2016-03-04 16:30:43 +0300

    File Storage

--- src/loc/linux/webapp/storage/FileStorage.java
+++ src/loc/linux/webapp/storage/FileStorage.java
@@ -1,10 +1,70 @@
 package loc.linux.webapp.storage;
 
-/**
- * Created by papa on 04.03.2016.
- */
-public class FileStorage extends IStorage {
+import loc.linux.webapp.WebAppExeption;
+import loc.linux.webapp.model.Resume;
 
-    public FileStorage(String dir) {
+import java.io.File;
+import java.util.List;
+
+
+public class FileStorage extends AbstractStorage<File> {
+    private File dir;
+
+    public FileStorage(String path) {
+        this.dir = new File(path);
+        if (!dir.isDirectory() || !dir.canWrite())
+            throw new IllegalArgumentException("'" + path + "' is not directory or is not writable");
+    }
+
+    @Override
+    protected void doClear() {
+        File[] files = dir.listFiles();
+        if (files == null) return;
+        for (File file : files) {
+            doDelete(file);
+            }
+        }
+
+    @Override
+    protected File getContext(String fileName) {
+        return new File(fileName);
+    }
+
+    @Override
+    protected boolean exist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected void doSave(File file, Resume r) {
+
+    }
+
+    @Override
+    protected void doDelete(File file) {
+        if (!file.delete()) {
+            throw new WebAppExeption("File " + file.getAbsolutePath() + " can not be deleted");
+        }
+    }
+
+    @Override
+    protected Resume doLoad(File file) {
+        return null;
+    }
+
+    @Override
+    protected void doUpdate(File file, Resume r) {
+
+    }
+
+
+    @Override
+    protected List<Resume> doGetAll() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
     }
 }
