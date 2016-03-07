2f42d771bb860a62c29994af474a8736b9084c62
Mikle Brednew <mbr0969@gmail.com>
2016-03-07 21:27:06 +0300

    TextSection 07.03.2016

--- src/loc/linux/webapp/model/TextSection.java
+++ src/loc/linux/webapp/model/TextSection.java
@@ -1,15 +1,41 @@
 package loc.linux.webapp.model;
 
+import java.util.Objects;
+
 /**
  * Created by papa on 19.01.2016.
  */
 public class TextSection extends Section {
     static final long serialVersionUID = 1L;
-    private String title;
-    private String comment;
 
+//    private String title;
+//    private String comment;
+    private String value;
 
-//    public TextSection(SectionType type) {
-//        super(type);
-//    }
+    public TextSection(String value) {
+       this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this==o) return true;
+        if(o == null || getClass() !=o.getClass()) return false;
+        TextSection that = (TextSection) o;
+
+        return !(value != null ? value.equals(that.value) : that.value != null);
+    }
+
+    @Override
+    public int hashCode(){
+        return value != null ? value.hashCode() : 0;
+    }
+
+    @Override
+    public String toString(){
+        return value;
+    }
 }
