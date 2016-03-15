9abf5c4a25dfdf7cd8c0ba4a65ad58a168faf935
Mikle Brednew <mbr0969@gmail.com>
2016-03-15 18:00:13 +0300

    WEB 15.03.2016 8.1 01:00

--- test/loc/linux/webapp/storage/AbstractStorageTest.java
+++ test/loc/linux/webapp/storage/AbstractStorageTest.java
@@ -20,7 +20,7 @@ import static org.junit.Assert.assertEquals;
 
 
 abstract public class AbstractStorageTest {
-    public static final String FILE_STORAGE = "/windows/Project/Web/webapp/file_storage";
+    public static final String FILE_STORAGE = "./file_storage";
     private Resume R1, R2, R3;
     protected IStorage storage;
 
