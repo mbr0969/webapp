d3fb23002c1377c584e3a4d0fe913d8c03ea76a3
mbr0969 <mbr0969@gmail.com>
2016-03-25 23:47:13 +0300

    WEB 25.03.2016 10.1 00:10 SQL

--- src/Lesson07/Main.java
+++ src/Lesson07/Main.java
@@ -9,6 +9,6 @@ public class Main {
         System.out.println(new Date().getTime());
         System.out.println(System.currentTimeMillis());
         System.out.println(new Date(Long.MAX_VALUE));
-        sldkjf
+
     }
 }
